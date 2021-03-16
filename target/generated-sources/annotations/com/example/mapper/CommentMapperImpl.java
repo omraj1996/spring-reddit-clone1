1d4482280c5ffa302a3bd38c89a4869e9b78a5c9
Om Raj <omraj.nitrr@gmail.com>
2021-03-16 11:43:03 +0530

    hjfskdkdhn

--- target/generated-sources/annotations/com/example/mapper/CommentMapperImpl.java
+++ target/generated-sources/annotations/com/example/mapper/CommentMapperImpl.java
@@ -10,7 +10,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2021-03-16T10:40:02+0530",
+    date = "2021-03-16T11:41:53+0530",
     comments = "version: 1.3.1.Final, compiler: javac, environment: Java 1.8.0_251 (Oracle Corporation)"
 )
 @Component
